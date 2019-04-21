d8db40908a3e44c55431f20690b2b55532c7a2bf
jormd <johann.romand@etudiant.univ-reims.fr>
2019-04-21 19:24:39 +0200

    test récupération series

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,6 +7,27 @@ const auth = new Auth();
 class Home extends Component{
     constructor(props) {
         super(props);
+        this.state = {
+            series : this.recuperationSerie()
+        }
+    }
+
+    recuperationSerie(){
+        var berar = 'Bearer '+auth.getToken();
+        fetch("http://127.0.0.1:8080/api/series/all", {
+            method: "GET",
+            headers: new Headers({
+                //'Access-Control-Allow-Origin': '*',
+                'Authorization': berar,
+                //'Content-Type': 'application/x-www-form-urlencoded', // <-- Specifying the Content-Type
+            }),
+        }).then(response => response.text())
+            .then(response => {
+                console.log(response);
+                if(response.code === "succes"){
+                    console.log(response);
+                }
+            });
     }
 
     render() {
@@ -14,6 +35,7 @@ class Home extends Component{
             <div class="main">
                 <p>aa</p>
                 <p>{auth.getToken()}</p>
+                <p>aa  : { this.state.series }</p>
             </div>
         );
     }
